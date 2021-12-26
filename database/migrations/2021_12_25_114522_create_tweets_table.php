b27656547860f95c8ee1c71b8391d8c7b0e17080
Abhishek Shukla <shuklaji88as@gmail.com>
2021-12-26 18:10:48 +0530

    updated at column added in migrations

--- database/migrations/2021_12_25_114522_create_tweets_table.php
+++ database/migrations/2021_12_25_114522_create_tweets_table.php
@@ -18,6 +18,7 @@ class CreateTweetsTable extends Migration
             $table->uuid('user_id');
             $table->text('content');
             $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
